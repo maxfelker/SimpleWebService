4eef66a31b3bc8cadfcb630ed0842b22c1b22093
MW Felker <mwadefelker@gmail.com>
2020-05-21 21:16:01 -0400

    Updated git ignore, removed directories and files

--- Assets/SimpleWebService/SimpleWebService.cs
+++ Assets/SimpleWebService/SimpleWebService.cs
@@ -1,66 +1,70 @@
 ﻿/*
- * Web Service
+ * Simple Web Service
  * 
- * A simple HTTP get / post wrapper built on SimpleJSON.
+ * A simple HTTP tool for Unity3D that interacts with JSON APIs via UnityWebRequest
  * 
- * Max Felker | felkerm@gmail.com
+ * https://github.com/mw-felker/SimpleWebService
  * 
 */
 
-using System; 
+using System;
 using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections; 
+using System.Collections;
 using System.Collections.Generic;
 using SimpleJSON;
 using System.Linq;
 
-public class SimpleWebService : MonoBehaviour 
+public class SimpleWebService : MonoBehaviour
 {
-		
-		// Set up our call back delegation, passing back a JSONNode obkect
-  	protected delegate void CallBack(JSONNode response);
-	
-		// Make a GET request inside a Coroutine and pass the callback function upon completion 
-		protected void Get(string URL, CallBack callback) => StartCoroutine(
-			StartRequest(UnityWebRequest.Get(URL), callback)
-		);
 
-		// Sent the HTTP request, parse the JSON response and fire the callback 
-    protected IEnumerator StartRequest( UnityWebRequest request, CallBack callback)
-		{
-				using (request) {
+  // Set up our call back delegation, passing back a JSONNode obkect
+  protected delegate void CallBack(JSONNode response);
 
-					yield return request.SendWebRequest();
+  // Make a GET request inside a Coroutine and pass the callback function upon completion 
+  protected void Get(string URL, CallBack callback) => StartCoroutine(
+    StartRequest(UnityWebRequest.Get(URL), callback)
+  );
 
-					if ( request.isNetworkError || request.isHttpError ) {
-						Debug.LogError(request.error);
-					}
+  // Sent the HTTP request, parse the JSON response and fire the callback 
+  protected IEnumerator StartRequest(UnityWebRequest request, CallBack callback)
+  {
+    using (request)
+    {
 
-					JSONNode responseJSON = JSON.Parse(request.downloadHandler.text);
+      yield return request.SendWebRequest();
 
-					if(callback != null) {
-						callback(responseJSON);
-					}
-					
-				}		
-		}
+      if (request.isNetworkError || request.isHttpError)
+      {
+        Debug.LogError(request.error);
+      }
 
-    // POST to the server
-    protected void Post(string URL, Dictionary<string,string> payload, CallBack callback)
-		{
+      JSONNode responseJSON = JSON.Parse(request.downloadHandler.text);
 
-        // create the new form		
-        WWWForm form = new WWWForm();
-		
-        // Add key,value pairs to POST array
-	    foreach(KeyValuePair<String,String> data in payload) {
-	   		form.AddField(data.Key, data.Value);
-	    }
-	    
-        // Post the values to the URL		 
-	    StartRequest(UnityWebRequest.Post(URL, form), callback);
+      if (callback != null)
+      {
+        callback(responseJSON);
+      }
 
     }
+  }
+
+  // POST to the server
+  protected void Post(string URL, Dictionary<string, string> payload, CallBack callback)
+  {
+
+    // create the new form		
+    WWWForm form = new WWWForm();
+
+    // Add key,value pairs to POST array
+    foreach (KeyValuePair<String, String> data in payload)
+    {
+      form.AddField(data.Key, data.Value);
+    }
+
+    // Post the values to the URL		 
+    StartRequest(UnityWebRequest.Post(URL, form), callback);
+
+  }
 
 }
